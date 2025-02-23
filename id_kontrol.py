4bc60c6dbececcf3241f07947e8c0b0f761e1962
nrdnay <aydogdunurdan@gmail.com>
2025-02-23 11:47:26 +0300

    Update id_kontrol.py
    replaced upper and numeric lists.

--- id_kontrol.py
+++ id_kontrol.py
@@ -1,23 +1,22 @@
 number_of_id = int(input())
 ids = []
+
 for i in range(number_of_id) :
     id = input().strip()
     ids.append(id)
 
-numeric = []
-upper =[]
 for id in ids :
     if len(id) != 10 or len(set(id)) != 10 or id.isalnum() == False :
       print("Invalid")
       continue
 
-    else :
-        
-      for i in id :
-          if i.isupper() :
-              upper.append(i)
-          elif i.isnumeric() :
-              numeric.append(i)
+    upper = []
+    numeric = []
+    for i in id :
+      if i.isupper() :
+        upper.append(i)
+      elif i.isnumeric() :
+        numeric.append(i)
 
     if len(upper) >= 2 and len(numeric) >= 3 :
       print("Valid")
